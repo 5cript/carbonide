eac580676f64ffa687a03889ec53ac413d6a8303
5cript <Tim06TR@gmail.com>
2015-11-29 03:50:15 +0100

    Implemented several response methods.

--- server/rest/connection.cpp
+++ server/rest/connection.cpp
@@ -1,19 +1,43 @@
 #include "connection.hpp"
 #include "server.hpp"
 
-namespace Factorem { namespace Server { namespace RestApi
+#include <fstream>
+#include <stdexcept>
+
+namespace Carbonide { namespace Server { namespace RestApi
 {
+//#######################################################################################################
+    uint64_t readFile(std::ifstream& reader, ResponseHeader& response)
+    {
+        if (!reader.good())
+            throw std::runtime_error("Could not open file.");
+
+        reader.seekg(0, reader.end);
+        auto size = reader.tellg();
+        reader.seekg(0, reader.beg);
+
+        if (size == 0)
+        {
+            response.responseCode = 204;
+            response.responseString = "No Content";
+        }
+
+        return size;
+    }
+//-------------------------------------------------------------------------------------------------------
 //#######################################################################################################
     RestConnection::RestConnection(RestServer* owner, UserId const& id)
         : owner_(owner)
         , id_(id)
         , stream_()
+        , request_()
+        , head_()
+        , bodySize_(0)
     {
     }
 //-------------------------------------------------------------------------------------------------------
     void RestConnection::free()
     {
-        stream_.close();
         owner_->deregisterClient(this);
     }
 //-------------------------------------------------------------------------------------------------------
@@ -26,7 +50,103 @@ namespace Factorem { namespace Server { namespace RestApi
     {
         return id_;
     }
+//-------------------------------------------------------------------------------------------------------
+    Request RestConnection::getRequest() const
+    {
+        return request_;
+    }
+//-------------------------------------------------------------------------------------------------------
+    Header RestConnection::getHeader() const
+    {
+        return head_;
+    }
+//-------------------------------------------------------------------------------------------------------
+    uint32_t RestConnection::getBodySize() const
+    {
+        return bodySize_;
+    }
+//-------------------------------------------------------------------------------------------------------
+    void RestConnection::readHead()
+    {
+        // read first line of request.
+        stream_ >> request_.requestType;
+        stream_ >> request_.url;
+        stream_ >> request_.httpVersion;
+
+        if (!stream_ || request_.httpVersion.substr(0, 5) != "HTTP/")
+        {
+            throw InvalidRequest("Request does not contain a valid HTTP request header.");
+        }
+
+        // validate http version
+        //------------------------------------------------------------
+        auto version = request_.httpVersion.substr(5, request_.httpVersion.length() - 5);
+
+        if (version != "1.0" && version != "1.1")
+            throw InvalidRequest("HTTP version is not supported");
+        //------------------------------------------------------------
+
+        // Process the response headers, which are terminated by a blank line.
+        std::string headerLine;
+        std::getline(stream_, headerLine);
+        while (std::getline(stream_, headerLine))
+        {
+            if (headerLine == "\r")
+                break;
+
+            auto pos = headerLine.find(":");
+            if (pos == std::string::npos)
+                throw InvalidRequest("HTTP header entry does not contain colon");
+
+            auto lhs = headerLine.substr(0, pos);
+            auto rhs = headerLine.substr(pos + 1, headerLine.length() - pos);
+
+            head_.entries[lhs] = rhs;
+        }
+
+        bodySize_ = stream_.rdbuf()->available();
+    }
+//-------------------------------------------------------------------------------------------------------
+    void RestConnection::sendBinaryFile(std::string const& fileName, ResponseHeader response)
+    {
+        std::ifstream reader(fileName);
+        auto size = readFile(reader, response);
+
+        response.responseHeaderPairs["Content-Type"] = "application/octet-stream";
+        response.responseHeaderPairs["Content-Length"] = std::to_string(size);
+        response.responseHeaderPairs["Connection"] = "close";
+
+        stream_ << response.toString();
+        stream_ << reader.rdbuf();
+        stream_.flush();
+    }
+//-------------------------------------------------------------------------------------------------------
+    void RestConnection::sendTextFile(std::string const& fileName, std::string contentType, ResponseHeader response)
+    {
+        using namespace std::literals;
+
+        std::ifstream reader(fileName);
+        auto size = readFile(reader, response);
+
+        response.responseHeaderPairs["Content-Type"] = "text/"s + contentType;
+        response.responseHeaderPairs["Content-Length"] = std::to_string(size);
+        response.responseHeaderPairs["Connection"] = "close";
+
+        stream_ << response.toString();
+        stream_ << reader.rdbuf();
+        stream_.flush();
+    }
+//-------------------------------------------------------------------------------------------------------
+    void RestConnection::sendString(std::string const& text, ResponseHeader response)
+    {
+        response.responseHeaderPairs["Content-Length"] = std::to_string(text.length());
+        response.responseHeaderPairs["Connection"] = "close";
+
+        stream_ << response.toString();
+        stream_ << text;
+        stream_.flush();
+    }
 //#######################################################################################################
 } // namespace RestApi
 } // namespace Server
-} // namespace Factorem
+} // namespace Carbonide
--- server/rest/connection.hpp
+++ server/rest/connection.hpp
@@ -3,33 +3,189 @@
 
 #include "user_id.hpp"
 #include "forward.hpp"
+#include "request.hpp"
+#include "header.hpp"
+#include "exceptions.hpp"
+#include "response.hpp"
 
+#ifndef Q_MOC_RUN // A Qt workaround, for those of you who use Qt
+#   include "../../lib/SimpleJSON/stringify/jss.h"
+#   include "../../lib/SimpleJSON/stringify/jss_fusion_adapted_struct.h"
+#endif
+
+#include <string>
 #include <memory>
 #include <boost/asio.hpp>
 
-namespace Factorem { namespace Server { namespace RestApi {
+namespace Carbonide { namespace Server { namespace RestApi {
 
+    /**
+     *  A REST connection to a client.
+     */
     class RestConnection : public std::enable_shared_from_this <RestConnection>
     {
+        friend RestServer;
+
     public:
-        RestConnection(RestServer* owner, UserId const& id);
         ~RestConnection() = default;
 
-        void free();
+        /**
+         *  Returns the socket iostream for writing.
+         *  Do not read the header from it.
+         *  It is recommended to call flush after writing everything to the stream.
+         *
+         *  @return A tcp::iostream. See boost asio documentation.
+         */
         boost::asio::ip::tcp::iostream& getStream();
+
+        /**
+         *  Returns the connected clients id.
+         *
+         *  @return Returns the user id.
+         */
         UserId getId() const;
+
+        /**
+         *  Returns the request that has been parsed.
+         *
+         *  @return A request containing all info present in the first line of a request.
+         */
+        Request getRequest() const;
+
+        /**
+         *  Returns the http header entries.
+         *
+         *  @return A header containing the key:value pairs.
+         */
+        Header getHeader() const;
+
+        /**
+         *  Returns the size of the body.
+         *
+         *  @return body size.
+         */
+        uint32_t getBodySize() const;
+
+        /**
+         *  Send JSON response. uses SimpleJSON library to stringify the object.
+         *  Automatically sets the following header key/value pairs
+         *
+         *  Content-Type: text/json; charset=UTF-8
+         *  Content-Length: ...
+         *  Connection: close
+         *
+         *  @param object An object to stringify.
+         *  @param responseHeader A response header containing header information,
+         *         such as response code, version and response message.
+         */
+        template <typename T>
+        void sendJson(T const& object, ResponseHeader response = {})
+        {
+            using namespace std::literals;
+
+            response.responseHeaderPairs["Content-Type"s] = "text/json; charset=UTF-8"s;
+
+            std::stringstream body;
+            body << '{';
+            JSON::try_stringify(body, "", object);
+            body << '}';
+
+            auto bodySize = body.tellp();
+            response.responseHeaderPairs["Content-Length"s] = std::to_string(bodySize);
+            response.responseHeaderPairs["Connection"s] = "close"s;
+
+            body.seekg(0);
+
+            stream_ << response.toString();
+            stream_ << body.rdbuf();
+            stream_.flush();
+        }
+
+        /**
+         *  Sends a file as binary contents.
+         *  The response will be 204 for empty files and whats provided otherwise,
+         *  which might be 200 if no explicit response header is provided.
+         *  It does not send error codes on itself, but throws when the file cannot be opened.
+         *
+         *  Automatically sets the following header key/value pairs
+         *
+         *  Content-Type: application/octet-stream
+         *  Content-Length: ...
+         *  Connection: close
+         *
+         *  @param fileName A file to send.
+         *  @param responseHeader A response header containing header information,
+         *         such as response code, version and response message.
+         */
+        void sendBinaryFile(std::string const& fileName, ResponseHeader response = {});
+
+        /**
+         *  Sends a file as text contents.
+         *  The response will be 204 for empty files and whats provided otherwise,
+         *  which might be 200 if no explicit response header is provided.
+         *  It does not send error codes on itself, but throws when the file cannot be opened.
+         *
+         *  Automatically sets the following header key/value pairs
+         *
+         *  Content-Type: text/contentType   // contentType is the parameter passed second.
+         *  Content-Length: ...
+         *  Connection: close
+         *
+         *  @param fileName A file to send.
+         *  @param contentType the text content type. Might by "html" for instance.
+         *  @param responseHeader A response header containing header information,
+         *         such as response code, version and response message.
+         */
+        void sendTextFile(std::string const& fileName, std::string contentType, ResponseHeader response = {});
+
+        /**
+         *  Sends a string. You must set the content type yourself on the response parameter.
+         *  Therefore the parameter is not optional.
+         *  The response will be 204 for empty strings and whats provided otherwise,
+         *  which might be 200 if no explicit response header is provided.
+         *
+         *  Automatically sets the following header key/value pairs
+         *
+         *  Content-Length: text.length()
+         *  Connection: close
+         *
+         *  @param text A text to send.
+         *  @param responseHeader A response header containing header information,
+         *         such as response code, version and response message.
+         */
+        void sendString(std::string const& text, ResponseHeader response);
+
+    private:
+        /**
+         *  Connections can only be created by a server.
+         *  Users shall never create a connection on their own,
+         *  this makes no sense.
+         */
+        RestConnection(RestServer* owner, UserId const& id);
+
+        /**
+         *  Reads and parses the head.
+         */
+        void readHead();
+
+        /**
+         *  Closes the connection and removes it from the server list.
+         */
+        void free();
 
     private:
         RestServer* owner_;
         UserId id_;
         boost::asio::ip::tcp::iostream stream_;
-        //boost::asio::ssl::context context_;
-        //boost::asio::ssl::stream <boost::asio::ip::tcp::socket> sslSock_;
+
+        Request request_;
+        Header head_;
+        uint32_t bodySize_;
     };
 
 } // namespace RestApi
 } // namespace Server
-} // namespace Factorem
+} // namespace Carbonide
 
 
 #endif // CONNECTION_HPP_INCLUDED
