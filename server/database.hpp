c322addce299e9b1f22f14c9463fe65070d9331d
5cript <Tim06TR@gmail.com>
2015-12-04 00:02:01 +0100

    Removed submodule

---
+++ server/database.hpp
@@ -0,0 +1,17 @@
+#ifndef DATABASE_HPP_INCLUDED
+#define DATABASE_HPP_INCLUDED
+
+
+
+namespace Carbonide { namespace Server {
+
+    class Database
+    {
+    private:
+
+    };
+
+} // namespace Server
+} // namespace Carbonide
+
+#endif // DATABASE_HPP_INCLUDED
