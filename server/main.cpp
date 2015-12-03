1d26737bdbad60bd9b7e19d2833a7eea01fbab66
5cript <Tim06TR@gmail.com>
2015-12-03 23:33:11 +0100

    Added sqlpp11 as submodule including sqlite3 connector.

--- server/main.cpp
+++ server/main.cpp
@@ -1,42 +1,18 @@
 #include <iostream>
 
-#include "../lib/SimpleREST/server.hpp"
-#include "../lib/SimpleREST/connection.hpp"
-#include "../lib/SimpleREST/mime.hpp"
+#include "../lib/SimpleREST/restful.hpp"
 
 #include <fstream>
 
 int main()
 {
-    using namespace RestApi;
+    using namespace Rest;
 
-    RestServer server([](std::shared_ptr <RestConnection> connection) {
-        //auto& stream = connection->getStream();
+    InterfaceProvider api(8081);
 
-        std::ofstream file ("./test.bmp", std::ios_base::binary);
-
-        std::cout << connection->getAddress() << ":" << connection->getPort() << "\n";
-
-        connection->readStream(file);
-
-        connection->sendFile("C:/Users/Tim/Desktop/September.pdf", true);
-
-
-    }, [](std::shared_ptr <RestConnection> connection, InvalidRequest const& erroneousRequest) {
-
-        ResponseHeader response;
-        response.responseCode = 400;
-        response.responseString = "Bad Request";
-        connection->sendHeader(response);
-
-        std::cerr << erroneousRequest.what() << "\n";
-    }, 8081);
-
-    server.start();
+    api.start();
 
     std::cin.get();
-
-    server.stop();
 
     return 0;
 }
