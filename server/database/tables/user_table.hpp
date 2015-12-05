57b75fe2555c34edfb17ad1964b807d2806fccdb
5cript <Tim06TR@gmail.com>
2015-12-05 18:29:59 +0100

    Added user and token table.

--- server/database/tables/user_table.hpp
+++ server/database/tables/user_table.hpp
@@ -1,11 +1,81 @@
-#ifndef DATABASE_TABLES_USER_TABLE_HPP_INCLUDED
-#define DATABASE_TABLES_USER_TABLE_HPP_INCLUDED
+#ifndef MYNAMESPACE_USER_TABLE_H
+#define MYNAMESPACE_USER_TABLE_H
 
-namespace Carbonide { namespace Server { namespace Database { namespace Tables {
+#include <sqlpp11/table.h>
+#include <sqlpp11/column_types.h>
+#include <sqlpp11/char_sequence.h>
 
-} // namespace Tables
-} // namespace Database
-} // namespace Server
-} // namespace Carbonide
+namespace MyNamespace
+{
+  namespace Users_
+  {
+    struct Id
+    {
+      struct _alias_t
+      {
+        static constexpr const char _literal[] =  "id";
+        using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;
+        template<typename T>
+        struct _member_t
+          {
+            T id;
+            T& operator()() { return id; }
+            const T& operator()() const { return id; }
+          };
+      };
+      using _traits = sqlpp::make_traits<sqlpp::integer, sqlpp::tag::can_be_null>;
+    };
+    struct Name
+    {
+      struct _alias_t
+      {
+        static constexpr const char _literal[] =  "name";
+        using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;
+        template<typename T>
+        struct _member_t
+          {
+            T name;
+            T& operator()() { return name; }
+            const T& operator()() const { return name; }
+          };
+      };
+      using _traits = sqlpp::make_traits<sqlpp::varchar, sqlpp::tag::require_insert>;
+    };
+    struct Token
+    {
+      struct _alias_t
+      {
+        static constexpr const char _literal[] =  "token";
+        using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;
+        template<typename T>
+        struct _member_t
+          {
+            T token;
+            T& operator()() { return token; }
+            const T& operator()() const { return token; }
+          };
+      };
+      using _traits = sqlpp::make_traits<sqlpp::integer, sqlpp::tag::can_be_null>;
+    };
+  }
 
-#endif // DATABASE_TABLES_USER_TABLE_HPP_INCLUDED
+  struct Users: sqlpp::table_t<Users,
+               Users_::Id,
+               Users_::Name,
+               Users_::Token>
+  {
+    struct _alias_t
+    {
+      static constexpr const char _literal[] =  "users";
+      using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;
+      template<typename T>
+      struct _member_t
+      {
+        T users;
+        T& operator()() { return users; }
+        const T& operator()() const { return users; }
+      };
+    };
+  };
+}
+#endif
